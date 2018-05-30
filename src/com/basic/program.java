aff5c6814428d2fb692c720c5ca14c71d1231598
stephenraj1442001 <stephen1442001@gmail.com>
2018-05-30 15:15:24 +0530

    program

---
+++ src/com/basic/program.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class program 
+	{
+	public static void main( String[] args )
+	{
+	  for ( int i = 1; i <= 20; i++ )
+	   {
+	     for ( int j = 1; j <= 10; j++ )
+	       System.out.print( '@' );
+
+	         System.out.println();
+	   } 
+	} 
+	
+
+	}
+
