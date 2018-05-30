ed69f1f0142a8b8c05485aaddbb7b1bad698774b
stephenraj1442001 <stephen1442001@gmail.com>
2018-05-30 15:06:27 +0530

    switchcase

---
+++ src/com/basic/switchcase.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class switchcase {
+	public static void main(String[] args)
+	{
+	  int count;
+	   switch (count = 2 )
+	   {
+	   case 1:
+		   System.out.println("1");
+		   break;
+	   case 2:
+		   System.out.println("2");
+		   break;
+		default:
+			  System.out.println("none");
+			  break;
+	   }	 
+	}	
+}
