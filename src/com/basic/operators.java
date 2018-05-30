6267491cd916106e9ec7874c6c61a524347cfd8e
stephenraj1442001 <stephen1442001@gmail.com>
2018-05-30 12:40:39 +0530

    operators

---
+++ src/com/basic/operators.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class operators {
+	public static void main(String[] args)
+	{
+		int a=10;
+		boolean istrue = false;
+		System.out.println(!istrue);
+		System.out.println(a < 20);
+		System.out.println(a > 20);
+		System.out.println(a<=50);
+		System.out.println(a>=50);
+		System.out.println(a==10);
+		System.out.println(a!=50);
+		System.out.println(5 & 6);
+		System.out.println(2 | 8);
+		System.out.println(1 ^ 3);
+		System.out.println(a == 10 & istrue);
+		System.out.println(a == 10 | istrue);
+	}
+
+}
