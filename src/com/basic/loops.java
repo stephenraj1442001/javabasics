2fd1765e27c4f6f945259ac4e1dd2c4b474b0869
stephenraj1442001 <stephen1442001@gmail.com>
2018-05-30 12:43:14 +0530

    loops

---
+++ src/com/basic/loops.java
@@ -0,0 +1,32 @@
+package com.basic;
+
+public class loops {
+	public static void main(String[] args)
+	{
+		int i = 1;
+		while ( i <= 10 );
+		{
+		++i;
+		}
+
+		int k;
+		for (k= 0; k <= 1.0; k++ )
+		{
+		System.out.println( k );
+		}
+		int n = 2;
+		switch (n)
+		{
+		case 1:
+		System.out.println( "The number is 1" );
+		case 2:
+		System.out.println( "The number is 2" );
+		break;
+		default:
+		System.out.println( "The number is not 1 or 2" );
+		break;
+		}
+
+	}
+
+}
