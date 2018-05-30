bb06dcfe1ba8719cda6cde467c5dff220a7e3708
stephenraj1442001 <stephen1442001@gmail.com>
2018-05-30 15:28:45 +0530

    program

--- src/com/basic/program.java
+++ src/com/basic/program.java
@@ -4,13 +4,19 @@ public class program
 	{
 	public static void main( String[] args )
 	{
-	  for ( int i = 1; i <= 20; i++ )
-	   {
-	     for ( int j = 1; j <= 10; j++ )
-	       System.out.print( '@' );
+		int i,j,k;
+		for (i= 1; i <= 5; i++ )
+		{
+		for (j= 1; j <= 3; j++ )
+		{
+		for (k= 1; k <= 4; k++ )
+		System.out.print( '*' );
+		System.out.println();
+		} // end inner for
+		System.out.println();
+		} // end outer for
 
-	         System.out.println();
-	   } 
+
 	} 
 	
 
