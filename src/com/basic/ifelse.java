e6cee9a9515c5c1a52ebd45c83ddc782ad221c6a
stephenraj1442001 <stephen1442001@gmail.com>
2018-05-30 14:44:11 +0530

    ifelse

---
+++ src/com/basic/ifelse.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class ifelse {
+	public static void main(String[] args)
+	{
+	  int count = 7;
+		if(count < 4)
+			System.out.println("true");
+		else
+			System.out.println("false");
+	}
+}
