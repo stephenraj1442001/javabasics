7a579e2092f13d1eb77077754b8f26e1093efac4
stephenraj1442001 <stephen1442001@gmail.com>
2018-06-04 17:08:45 +0530

    Reverse numbers

---
+++ src/com/basic/ReverseArray.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class ReverseArray {
+	public static void main(String[] args) {
+		int a[] = {3,4,5,33,24,2},i=0;
+     for (int j = a.length-1 ; j >= 0 ; j-- )
+	    	 {
+	    		 i = a[j];
+	    		 System.out.print(" "+i);
+	    	 }
+	     
+	
+	 
+	
+	}
+	
+	
+}
