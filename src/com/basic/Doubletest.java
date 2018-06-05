485cb0859e25878fe31ad739566a7b3b3148521d
stephenraj1442001 <stephen1442001@gmail.com>
2018-06-05 19:00:44 +0530

    parse Double program

---
+++ src/com/basic/Doubletest.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+
+	public class Doubletest
+	{
+	    public static void main(String args[])
+	    {
+	        double total = 0.0;
+	            
+	       
+	            double d = Double.parseDouble(args[0]);
+	            double d1 = Double.parseDouble(args[1]);
+
+	            total = d + d1;
+	      
+	        System.out.println( total);
+	    }
+	}
+
